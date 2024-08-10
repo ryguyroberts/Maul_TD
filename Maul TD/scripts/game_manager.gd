8e041592d5e0627d2cfd418c93533db58cfa3310
Ryan <ryguyrobertson@gmail.com>
2024-08-10 16:27:38 -0700

    add click placing non-nav tile

--- Maul TD/scripts/game_manager.gd
+++ Maul TD/scripts/game_manager.gd
@@ -1,13 +1,35 @@
-extends Node
+extends Node2D
 
+@onready var tile_map : TileMap = $TileMap
 
-@onready var tile_map = $TileMap
-
+# Declare Variables
+var ground_layer = 0
 
 func _ready():
 	pass
 		
+		
 func _input(event):
-	if Input.is_action_just_pressed("click"):
-		print("Click") 
+	# Testing Input
+	if Input.is_action_just_pressed("left_click"):
+		
+		# Get mouse position, convert global -> local for cleaner integers
+		var mouse_pos : Vector2 = get_global_mouse_position()
+		var tile_mouse_pos : Vector2i = tile_map.local_to_map(mouse_pos)
+		# Grass Tileset
+		var source_id : int = 0
+		# Non-Navigation Layer grass tile
+		var atlas_coord : Vector2i = Vector2i(24,5)
+		
+		# 24, 5 
+		
+		tile_map.set_cell(ground_layer, tile_mouse_pos, source_id, atlas_coord)
+		
+		#var tile_mouse_pos = tile_map.local_to_map(mouse_pos)
+		
+		#print ("local position", tile_mouse_pos)
 	
+	#if Input.is_action_just_pressed("right_click"):
+		#print("R_Click")
+
+
