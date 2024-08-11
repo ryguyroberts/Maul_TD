e953f1da059623eb314b246657e47f101c479621
Ryan <ryguyrobertson@gmail.com>
2024-08-11 10:05:59 -0700

    update method for path block checking

--- Maul TD/scripts/pathtestmarker.gd
+++ Maul TD/scripts/pathtestmarker.gd
@@ -3,23 +3,23 @@ extends Marker2D
 @export var marker_end_zone: Marker2D
 @onready var nav_agent : NavigationAgent2D = $NavigationAgent2D
 
-
-
-# Load Nav agent
-# Set a target point for nav agent
-
-func _ready():
-	makepath()
-	
-# Create a function that can be called to run the path,
-# Potentially the method that returns parents co-ordinates if no route?
 func makepath():
-	nav_agent.target_position = marker_end_zone.global_position
-	var final_pos : Vector2 = nav_agent.get_final_position()
-	print("final_pos", final_pos)
+	# Set nav agent target to end zone marker
+	var end_zone_global = marker_end_zone.global_position
+	nav_agent.target_position = end_zone_global
+	
+	#True final location
+	const final_reachable_position: Vector2 = Vector2(1,435)
+	var nav_final_pos : Vector2 = nav_agent.get_final_position()
+	
+	if nav_final_pos == final_reachable_position:
+		print("Path is clear")
+	else:
+		print("Path Blocked at", nav_final_pos)
+	
+
 
 # Test function with Timer,
-
 # Then call the method when we are placing a tower.
 
 func _on_timer_timeout():
