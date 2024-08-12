cc15a824a632fd3783fa69e792ee0c5f829792fe
Ryan <ryguyrobertson@gmail.com>
2024-08-11 21:37:50 -0700

    add bow anims + rotation reset

--- Maul TD/scripts/bow_1.gd
+++ Maul TD/scripts/bow_1.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
+var default_rotation = PI / 2
+# 20.43
+@onready var animated_sprite : AnimatedSprite2D = $"2DWeaponPivot/AnimatedSprite2D"
+
+
 func _physics_process(delta):
 	var enemies_in_range: Array[Node2D] = get_overlapping_bodies()
 	
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+		animated_sprite.play("shoot")
+	else:
+		rotation = default_rotation
+		animated_sprite.play("idle")
